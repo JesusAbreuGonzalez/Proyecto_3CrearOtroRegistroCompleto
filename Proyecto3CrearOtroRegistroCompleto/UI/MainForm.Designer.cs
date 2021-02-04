1cd8e7c55286ab46abb6a25f63a783d6deb5feba
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-02-04 01:42:37 -0400

    Agregando nuevamente un menú

--- Proyecto3CrearOtroRegistroCompleto/UI/MainForm.Designer.cs
+++ Proyecto3CrearOtroRegistroCompleto/UI/MainForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Proyecto3CrearOtroRegistroCompleto.UI
+namespace Proyecto3CrearOtroRegistroCompleto
 {
     partial class MainForm
     {
@@ -29,49 +29,51 @@ namespace Proyecto3CrearOtroRegistroCompleto.UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.RegistroUsuarioButton = new System.Windows.Forms.Button();
-            this.RegistrarRolButton = new System.Windows.Forms.Button();
-            this.OpcionLabel = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.RegistroRolesButton = new System.Windows.Forms.Button();
+            this.RegistroUsuariosButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // RegistroUsuarioButton
+            // label1
             // 
-            this.RegistroUsuarioButton.Location = new System.Drawing.Point(329, 98);
-            this.RegistroUsuarioButton.Name = "RegistroUsuarioButton";
-            this.RegistroUsuarioButton.Size = new System.Drawing.Size(155, 84);
-            this.RegistroUsuarioButton.TabIndex = 0;
-            this.RegistroUsuarioButton.Text = "Registrar Usuario";
-            this.RegistroUsuarioButton.UseVisualStyleBackColor = true;
-            this.RegistroUsuarioButton.Click += new System.EventHandler(this.RegistroUsuarioButton_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(55, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Presione la opción deseada";
             // 
-            // RegistrarRolButton
+            // RegistroRolesButton
             // 
-            this.RegistrarRolButton.Location = new System.Drawing.Point(32, 98);
-            this.RegistrarRolButton.Name = "RegistrarRolButton";
-            this.RegistrarRolButton.Size = new System.Drawing.Size(155, 84);
-            this.RegistrarRolButton.TabIndex = 1;
-            this.RegistrarRolButton.Text = "Registrar Rol";
-            this.RegistrarRolButton.UseVisualStyleBackColor = true;
-            this.RegistrarRolButton.Click += new System.EventHandler(this.RegistrarRolButton_Click);
+            this.RegistroRolesButton.Location = new System.Drawing.Point(25, 99);
+            this.RegistroRolesButton.Name = "RegistroRolesButton";
+            this.RegistroRolesButton.Size = new System.Drawing.Size(123, 75);
+            this.RegistroRolesButton.TabIndex = 1;
+            this.RegistroRolesButton.Text = "Registro de roles";
+            this.RegistroRolesButton.UseVisualStyleBackColor = true;
+            this.RegistroRolesButton.Click += new System.EventHandler(this.RegistroRolesButton_Click);
             // 
-            // OpcionLabel
+            // RegistroUsuariosButton
             // 
-            this.OpcionLabel.AutoSize = true;
-            this.OpcionLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.OpcionLabel.Location = new System.Drawing.Point(96, 33);
-            this.OpcionLabel.Name = "OpcionLabel";
-            this.OpcionLabel.Size = new System.Drawing.Size(355, 38);
-            this.OpcionLabel.TabIndex = 2;
-            this.OpcionLabel.Text = "Presione la opción deseada";
+            this.RegistroUsuariosButton.Location = new System.Drawing.Point(301, 99);
+            this.RegistroUsuariosButton.Name = "RegistroUsuariosButton";
+            this.RegistroUsuariosButton.Size = new System.Drawing.Size(123, 75);
+            this.RegistroUsuariosButton.TabIndex = 2;
+            this.RegistroUsuariosButton.Text = "Registro de usuarios";
+            this.RegistroUsuariosButton.UseVisualStyleBackColor = true;
+            this.RegistroUsuariosButton.Click += new System.EventHandler(this.RegistroUsuariosButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 211);
-            this.Controls.Add(this.OpcionLabel);
-            this.Controls.Add(this.RegistrarRolButton);
-            this.Controls.Add(this.RegistroUsuarioButton);
+            this.ClientSize = new System.Drawing.Size(448, 195);
+            this.Controls.Add(this.RegistroUsuariosButton);
+            this.Controls.Add(this.RegistroRolesButton);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
@@ -81,8 +83,8 @@ namespace Proyecto3CrearOtroRegistroCompleto.UI
 
         #endregion
 
-        private System.Windows.Forms.Button RegistroUsuarioButton;
-        private System.Windows.Forms.Button RegistrarRolButton;
-        private System.Windows.Forms.Label OpcionLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button RegistroRolesButton;
+        private System.Windows.Forms.Button RegistroUsuariosButton;
     }
 }
--- Proyecto3CrearOtroRegistroCompleto/UI/MainForm.cs
+++ Proyecto3CrearOtroRegistroCompleto/UI/MainForm.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Proyecto3CrearOtroRegistroCompleto.UI
+namespace Proyecto3CrearOtroRegistroCompleto
 {
     public partial class MainForm : Form
     {
@@ -17,16 +17,16 @@ namespace Proyecto3CrearOtroRegistroCompleto.UI
             InitializeComponent();
         }
 
-        private void RegistrarRolButton_Click(object sender, EventArgs e)
+        private void RegistroRolesButton_Click(object sender, EventArgs e)
         {
-            Registros.RegistroRoles ventana = new Registros.RegistroRoles();
-            ventana.Show();
+            UI.Registros.RegistroRoles registroRoles = new UI.Registros.RegistroRoles();
+            registroRoles.Show();
         }
 
-        private void RegistroUsuarioButton_Click(object sender, EventArgs e)
+        private void RegistroUsuariosButton_Click(object sender, EventArgs e)
         {
-            RegistroUsuarios ventana = new RegistroUsuarios();
-            ventana.Show();
+            RegistroUsuarios registroUsuarios = new RegistroUsuarios();
+            registroUsuarios.Show();
         }
     }
 }
