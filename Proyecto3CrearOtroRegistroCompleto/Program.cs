59e217afcf96fc0404fbc68fe037a5605704ca72
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-02-04 20:38:56 -0400

    Estableciendo MainForm como principal en la ejecución

--- Proyecto3CrearOtroRegistroCompleto/Program.cs
+++ Proyecto3CrearOtroRegistroCompleto/Program.cs
@@ -17,7 +17,7 @@ namespace Proyecto3CrearOtroRegistroCompleto
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegistroUsuarios());
+            Application.Run(new MainForm());
         }
     }
 }
